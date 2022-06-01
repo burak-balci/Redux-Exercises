6074c4fcb9b2c5484f54355fda91e2efec9cb7c2
Burak <96511762+burak-balci@users.noreply.github.com>
2022-06-01 11:57:17 +0300

    Counter-app is done.

---
+++ counter-app/src/components/Counter.js
@@ -0,0 +1,35 @@
+import { useSelector, useDispatch } from "react-redux";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from "../redux/counter/counterSlice";
+import { useState } from "react";
+
+function Counter() {
+  const countValue = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  const [amount, setAmount] = useState(3);
+
+  return (
+    <div>
+      <h1>{countValue}</h1>
+
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+
+      <br />
+      <br />
+      <input
+        type="number"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <button onClick={() => dispatch(incrementByAmount(amount))}>
+        Increment by Amount
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
